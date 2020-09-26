46c8ec2833d06babd1be0509741ba9925de23c67
vitor.marques <vitor.marques@gatec.com.br>
2020-09-26 10:31:53 -0300

    Modified Game1. Started Game2

--- Game1-Pong/Ball.gd
+++ Game1-Pong/Ball.gd
@@ -1,34 +1,45 @@
 extends KinematicBody2D
 
 const SCREEN_WIDTH = 1280
-const SCREEN_PERCENTAGE_IN_ONE_SECOND = 0.15
+const SCREEN_PERCENTAGE_IN_ONE_SECOND = 0.13
 const SPEED = SCREEN_WIDTH * SCREEN_PERCENTAGE_IN_ONE_SECOND
+const ADDITIONAL_SPEED = 0.033 * SCREEN_WIDTH
 
+var hit_counter = 0
+var max_hits = 3
+
+var initial_direction = Vector2(1, 0)
 var direction = Vector2()
-var velocity = Vector2()
 
 
 func _ready():
-	direction.x = randf()*2.0 - 1
-	direction.y = randf()*2.0 - 1
-	
-	direction = direction.normalized()
+	random_direction()
+
 
 func _physics_process(delta):
 	
-	velocity = direction * SPEED
+	var velocity = direction * (SPEED + hit_counter * ADDITIONAL_SPEED)
 	
 	var collision_info = move_and_collide(velocity * delta)
 	if collision_info:
 		
-		var normal = collision_info.normal
-		normal.x = round(normal.x)
-		normal.y = round(normal.y)
-		
-		if normal.x != 0:
-			direction.x *= -1
-		
-		if normal.y != 0:
-			direction.y *= -1
-		
-		direction = direction.normalized()
+		if collision_info.collider.is_in_group("walls"):
+			direction = direction.bounce(collision_info.normal)
+			hit_counter = max(0, hit_counter - 0.2)
+			
+		elif collision_info.collider.is_in_group("paddles"):
+			direction = direction.bounce(collision_info.normal)
+			var y = direction.y/2 + collision_info.collider_velocity.normalized().y
+			direction = Vector2(direction.x ,y).normalized()
+			
+			hit_counter = min(max_hits, hit_counter + 1)
+
+
+func random_direction():
+	randomize()
+	var rand_angle = (randf() * 2*PI/3) - PI/3
+	
+	direction = initial_direction.rotated(rand_angle)
+	direction.x = (randi() & 2) - 1
+	
+	direction = direction.normalized()
--- Game1-Pong/Player.gd
+++ Game1-Pong/Player.gd
@@ -1,20 +1,41 @@
 extends KinematicBody2D
 
 const SCREEN_HEIGHT = 720
-const SCREEN_PERCENTAGE_PER_SECOND = 0.25
+const SCREEN_PERCENTAGE_PER_SECOND = 0.1
 const SPEED = SCREEN_HEIGHT * SCREEN_PERCENTAGE_PER_SECOND
+
+const TIME_TO_MAX_SPEED = 0.03
+const ACC = SPEED / TIME_TO_MAX_SPEED
 
 export (int) var player_index = 1
 
 var velocity = Vector2()
+var acceleration = Vector2()
+
 
 func _physics_process(delta):
 	
+	var min_speed = 0
+	var max_speed = 0
+	
 	if Input.is_action_pressed("down_"+str(player_index)):
-		velocity.y = SPEED
+		acceleration.y = ACC
+		if velocity.y < 0:
+			velocity.y = 0
+		min_speed = 0
+		max_speed = SPEED
+		
 	elif Input.is_action_pressed("up_"+str(player_index)):
-		velocity.y = -SPEED
+		acceleration.y = -ACC
+		if velocity.y > 0:
+			velocity.y = 0
+		min_speed = -SPEED
+		max_speed = 0
+		
 	else:
+		acceleration.y = 0
 		velocity.y = 0
+	
+	velocity.y += clamp(velocity.y + acceleration.y * delta, min_speed, max_speed)
 	
 	move_and_collide(velocity * delta)
