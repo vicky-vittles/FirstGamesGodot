54df32ac7e38d47e3ce1a9c1c380f634bff351f1
vitor.marques <vitor.marques@gatec.com.br>
2021-03-22 18:29:19 -0300

    Game 11: Added GUI effects (Game start, game over, level update)

--- Game11-SlimeClimb/main/game/GUI.gd
+++ Game11-SlimeClimb/main/game/GUI.gd
@@ -1,6 +1,19 @@
 extends CanvasLayer
 
 onready var current_stage = $root/CurrentStage
+onready var click_to_jump = $root/Position2D/ClickToJump
+onready var animation_player = $AnimationPlayer
+onready var game_over = $root/GameOver
 
 func update_stage(level_id: int):
 	current_stage.text = tr("LEVEL_LABEL") % str(level_id).pad_zeros(2)
+	animation_player.queue("new_level")
+
+func _on_Game_game_started():
+	animation_player.stop()
+	animation_player.queue("game_started")
+
+func _on_Game_game_ended():
+	animation_player.stop()
+	animation_player.queue("game_ended")
+	get_tree().paused = true
--- Game11-SlimeClimb/main/game/game.gd
+++ Game11-SlimeClimb/main/game/game.gd
@@ -1,18 +1,14 @@
 extends Node2D
 
+signal game_started()
+signal game_ended()
+
 onready var background = $Background
 onready var scrolling_level = $ScrollingLevel
 
 var slime_made_first_jump : bool = false
 
-func start():
-	background.start()
-	scrolling_level.start()
-
 func _on_Slime_jump_started():
 	if not slime_made_first_jump:
-		start()
+		emit_signal("game_started")
 	slime_made_first_jump = true
-
-func _on_DeathTrigger_player_died():
-	print("death trigger")
