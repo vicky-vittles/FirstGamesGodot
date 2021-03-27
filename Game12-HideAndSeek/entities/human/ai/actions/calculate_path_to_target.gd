ba458cc27fb907cb0bddf93d85de06f1ad6107c7
vitor.marques <vitor.marques@gatec.com.br>
2021-03-27 18:17:14 -0300

    Game12: Advancements on forest level

--- Game12-HideAndSeek/entities/human/ai/actions/calculate_path_to_target.gd
+++ Game12-HideAndSeek/entities/human/ai/actions/calculate_path_to_target.gd
@@ -7,7 +7,7 @@ var path
 func tick(actor, blackboard):
 	var my_pos = actor.global_transform.origin
 	var player_pos = blackboard.get(Params.TARGET_POS)
-	path = actor.nav.get_simple_path(my_pos, player_pos * NOT_Y_AXIS, true)
+	path = actor.nav.get_simple_path(my_pos, player_pos, false)
 	if path.size() > 0:
 		blackboard.set(Params.PATH_TO_TARGET, path)
 		blackboard.set(Params.PATH_TO_TARGET_CURRENT_NODE, 0)
